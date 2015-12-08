c43f87e47c4d3a21c2d1b870db994a47af7044e2
maratkanov-a <a.s.maratkanov@gmail.com>
2015-12-08 15:49:33 +0300

    added mysql

--- ask_project/urls.py
+++ ask_project/urls.py
@@ -8,6 +8,7 @@ from django.conf.urls.static import static
 urlpatterns = [
 
     url(r'^$', QuestionListView.as_view(), name='home-page'),
+    url(r'^hot/', QuestionListViewHot.as_view(), name='hot-page'),
     # authentication
     url(r'^login/', 'django.contrib.auth.views.login', name='login-page'),
     url(r'^logout/', 'django.contrib.auth.views.logout', {'next_page': '/'}, name='logout-page'),
--- hw_wsgi.py
+++
@@ -1,5 +0,0 @@
-def application(environ , start_response):
-    start_response('200', 'OK', [('Content-Type', 'text/html')])
-    return ['HELOOO!']
-
-# найти конфиг wsgi
